07b59d3bbef60ba658686d9488f843e5f928debc
bcherry4 <bcherry4@ford.com>
2022-04-08 14:50:18 -0600

    Add final changes

--- site/pokemon.js
+++ site/pokemon.js
@@ -18,7 +18,30 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${queryString.get("pokemon")}`)
       <h2>Abilities</h2>
         `
         main.append(pokemonDetails)
+        const abilitiesRequests = pokemon.abilities
+            .map(response => response.ability.url)
+            .map(url => {
+                return fetch(url).then(response => response.json())
+            })
+        return Promise.all(abilitiesRequests)
+    }).then(listOfAbilities => {
+        const ul = document.createElement("ul")
+        ul.classList = "abilities"
+        main.append(ul)
+        listOfAbilities.map(ability => {
+            const li = document.createElement("li")
+            li.innerHTML = `
+            <span class="ability-name">${ability.name[0].toUpperCase()}${ability.name.slice(1)}</span>
+            <span class="ability-short-description">${ability.effect_entries[1].short_effect}</span>
+            `
+            return li;
+        }).forEach(li => {
+            ul.append(li)
+        })
+        const spinner = document.querySelector(".spinner")
+        spinner.classList.add("hidden")
     })
+
 
 
 /* const main = document.querySelector("main");
