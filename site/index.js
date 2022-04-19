87962a2be3f52722d229a713954a9abac479aadd
bcherry4 <bcherry4@ford.com>
2022-04-19 08:54:22 -0600

    Remove log statements Add indentation

--- site/index.js
+++ site/index.js
@@ -17,7 +17,6 @@ fetch(url)
     .then(response => {
         return response.json()
     }).then(parsedResponse => {
-        console.log(parsedResponse)
         const urls = parsedResponse.results.map(result => result.url)
         const fetches = urls.map(url => fetch(url).then(response => response.json()))
         return Promise.all(fetches)
--- site/pokemon.js
+++ site/pokemon.js
@@ -5,17 +5,16 @@ const main = document.querySelector("main")
 fetch(`https://pokeapi.co/api/v2/pokemon/${queryString.get("pokemon")}`)
     .then(response => response.json())
     .then(pokemon => {
-        console.log(pokemon)
         const name = `${pokemon.name[0].toUpperCase()}${pokemon.name.slice(1)}`;
         const pokemonDetails = document.createElement("div")
         pokemonDetails.classList = "pokemon-details"
         pokemonDetails.innerHTML = `
         <figure>
-        <img src="${pokemon.sprites.front_shiny}" alt="${name}" />
-        <figcaption>${name}</figcaption>
+            <img src="${pokemon.sprites.front_shiny}" alt="${name}" />
+            <figcaption>${name}</figcaption>
         </figure>
     
-      <h2>Abilities</h2>
+        <h2>Abilities</h2>
         `
         main.append(pokemonDetails)
         const abilitiesRequests = pokemon.abilities
@@ -31,8 +30,8 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${queryString.get("pokemon")}`)
         listOfAbilities.map(ability => {
             const li = document.createElement("li")
             li.innerHTML = `
-            <span class="ability-name">${ability.name[0].toUpperCase()}${ability.name.slice(1)}</span>
-            <span class="ability-short-description">${ability.effect_entries[1].short_effect}</span>
+                <span class="ability-name">${ability.name[0].toUpperCase()}${ability.name.slice(1)}</span>
+                <span class="ability-short-description">${ability.effect_entries[1].short_effect}</span>
             `
             return li;
         }).forEach(li => {
